0ad115a0bf4a9781cf463dfcf4f83197d94db72f
PiperMH <piper.1.hayden@gmail.com>
2017-08-18 14:15:25 -0500

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,16 +19,16 @@ var crystals = {
 };
 
 //Score variables
-var currentPoints =0;
-var targetPoints =0;
+var currentPoints = 0;
+var targetPoints = 0;
 
 //Win/Losses Variables
 var winCount = 0;
 var lossCount = 0;
 
 //Functions
-var random =  function(min, max){
-	targetPoints = Math.floor(Math.random() * (max - min+ 1)) + min;
+var takeRandom =  function(min, max){
+	return Math.floor(Math.random() * (max - min+ 1)) + min;
 }
 
 var startGame = function(){
@@ -37,36 +37,64 @@ var startGame = function(){
 	currentPoints = 0;
 
 	//sets a new score to achieve when playing the game
-	targetPoints = Random(19, 120);
+	targetPoints = takeRandom(19, 120);
 
 	//sets value to crystals
-	crystals.blue.value = random(1,12);
-	crystals.red.value = random(1,12);
-	crystals.yellow.value = random(1,12);
-	crystals.green.value = random(1,12);
+	crystals.blue.value = takeRandom(1,12);
+	crystals.red.value = takeRandom(1,12);
+	crystals.yellow.value = takeRandom(1,12);
+	crystals.green.value = takeRandom(1,12);
 
 	//change html to reflect
 	$('#yourPoints').html(currentPoints);
 	$('#targetPoints').html(targetPoints);
-
 	
 }
 
+var addValues = function(crystals){
+
+	currentPoints = currentPoints + crystals.value;
+
+	$('#yourPoints').html(currentPoints);
+
+	win();
+}
+
+var win = function(){
+	
+	if(currentPoints > targetPoints){
+		lossCount++;
+		$("#loss").html(lossCount);
+
+		startGame();
+
+	} else if (currentPoints === targetPoints){
+		winCount++;
+		$("#win").html(winCount);
+
+		startGame();
+	}
+
+}
+
+
+
 //Click Process
 $('#blue').click(function(){
-
+	addValues(crystals.blue);
 });
 
 $('#red').click(function(){
-
+	addValues(crystals.red);
 });
 
 $('#yellow').click(function(){
-
+	addValues(crystals.yellow);
 });
 
 $('#green').click(function(){
-
+	addValues(crystals.green);
 });
 
+startGame();
 
